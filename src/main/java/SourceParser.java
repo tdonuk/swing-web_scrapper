2a51f42647f365dedb503ba06a8f3ab86ca78a60
Taha Donuk <rtdonuk@gmail.com>
2021-05-20 12:48:44 +0300

    File Paths adjusted to make program run after 'installation'

--- src/main/java/SourceParser.java
+++ src/main/java/SourceParser.java
@@ -2,6 +2,9 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -9,18 +12,29 @@ import java.util.Comparator;
 public class SourceParser {
     private File sourcesFile,preferredSourcesFile;
     private ArrayList<String> lines;
+    Path programData;
     private ArrayList<Website> sourceList;
     private Website site;
     private String wholeText;
-    private String currentDir;
+    private String currentDir, programDataDir;
     private Image img;
     private Icon icon;
 
     public SourceParser() {
         currentDir = System.getProperty("user.dir");
         sourcesFile = new File(currentDir+"\\resources\\data\\sources.data");
-        preferredSourcesFile = new File(currentDir+"\\resources\\data\\preferred_sources.data");
 
+
+        programDataDir = System.getenv("ProgramData");
+        programData = Paths.get(programDataDir);
+
+        File s = new File(programDataDir+"\\LastNews\\data\\");
+
+        if(!s.exists()) {
+            s.mkdirs();
+        }
+
+        preferredSourcesFile = new File(s+"\\preferred_sources.data");
 
         lines = new ArrayList<>();
     }
@@ -30,17 +44,13 @@ public class SourceParser {
     }
 
     public ArrayList<Website> parseSources(File file) {
-        try(FileReader fr = new FileReader(file);
-            BufferedReader br = new BufferedReader(fr)){
+        try{
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
 
-            while(br.ready()) {
-                lines.add(br.readLine());
-            }
+            wholeText = Files.readString(file.toPath());
 
-            wholeText = "";
-            for(String s : lines) {
-                wholeText = wholeText + s + "\n";
-            }
+            wholeText = wholeText.replaceAll("\\r\\n|\\n", "");
 
             sourceList = new ArrayList<>();
 
@@ -54,39 +64,39 @@ public class SourceParser {
 
                 site = new Website();
 
-                data = s.substring(s.indexOf("name: "), s.indexOf("\nmain-url: ")); //name of the website
+                data = s.substring(s.indexOf("name: "), s.indexOf("main-url: ")); //name of the website
                 data = data.substring(data.indexOf(" ")+1);
                 site.setName(data);
 
-                data = s.substring(s.indexOf("main-url: "),s.indexOf("\nheaders-url: ")); //url to the main page of the website
+                data = s.substring(s.indexOf("main-url: "),s.indexOf("headers-url: ")); //url to the main page of the website
                 data = data.substring(data.indexOf(" ")+1);
                 site.setMainUrl(data);
 
-                data = s.substring(s.indexOf("headers-url: "),s.indexOf("\nmain-list-address: "));  //url to the endpoint where we get headers
+                data = s.substring(s.indexOf("headers-url: "),s.indexOf("main-list-address: "));  //url to the endpoint where we get headers
                 data = data.substring(data.indexOf(" ")+1);
                 site.setHeadersUrl(data);
 
-                data = s.substring(s.indexOf("main-list-address: "),s.indexOf("\nheader-address: ")); //address of the header list
+                data = s.substring(s.indexOf("main-list-address: "),s.indexOf("header-address: ")); //address of the header list
                 data = data.substring(data.indexOf(" ")+1);
                 site.setMainListAddress(data);
 
-                data = s.substring(s.indexOf("header-address: "),s.indexOf("\nheader-link-address: ")); //address of the each header in the header list
+                data = s.substring(s.indexOf("header-address: "),s.indexOf("header-link-address: ")); //address of the each header in the header list
                 data = data.substring(data.indexOf(" ")+1);
                 site.setHeaderAddress(data);
 
-                data = s.substring(s.indexOf("header-link-address: "),s.indexOf("\ncontent-header-address: ")); //address for the link of the header
+                data = s.substring(s.indexOf("header-link-address: "),s.indexOf("content-header-address: ")); //address for the link of the header
                 data = data.substring(data.indexOf(" ")+1);
                 site.setHeaderLinkAddress(data);
 
-                data = s.substring(s.indexOf("content-header-address: ") , s.indexOf("\ncontent-details-address: ")); //content subtitle address (if exists)
+                data = s.substring(s.indexOf("content-header-address: ") , s.indexOf("content-details-address: ")); //content subtitle address (if exists)
                 data = data.substring(data.indexOf(" ")+1);
                 site.setContentHeaderAddress(data);
 
-                data = s.substring(s.indexOf("content-details-address: "),s.indexOf("\nicon: ")); //content detail address
+                data = s.substring(s.indexOf("content-details-address: "),s.indexOf("icon: ")); //content detail address
                 data = data.substring(data.indexOf(" ")+1);
                 site.setContentDetailsAddress(data);
 
-                data = s.substring(s.indexOf("icon: "),s.length()-1); //icon file name
+                data = s.substring(s.indexOf("icon: ")); //icon file name
                 data = data.substring(data.indexOf(" ")+1);
                 site.setImageFileName(data);
 
@@ -95,8 +105,11 @@ public class SourceParser {
 
                 sourceList.add(site);
             }
+
+            br.close();
+            fr.close();
         } catch(IOException e) {
-            showFileError(file.getName());
+            showFileError(file.toPath().toString());
         }
 
         Collections.sort(sourceList, Comparator.comparing(Website::getName));
@@ -122,7 +135,8 @@ public class SourceParser {
             }
         }
 
-        try(FileWriter fw = new FileWriter(preferredSourcesFile,false)) {
+        try {
+            FileWriter fw = new FileWriter(preferredSourcesFile,false);
             StringBuffer sb = new StringBuffer();
             for(Website w : list) {
                 sb.append("<source>\n");
@@ -138,6 +152,8 @@ public class SourceParser {
                 sb.append("</source>\n");
             }
             fw.write(sb.toString());
+            fw.close();
+
         } catch (IOException e) {
             showFileError(preferredSourcesFile.getName());
         }
@@ -147,8 +163,9 @@ public class SourceParser {
         return parseSources(preferredSourcesFile);
     }
 
-    private void showFileError(String fileName) {
-        String errorMessage = fileName + " not found. Please be sure the program executable and resources folder are in the same folder";
+    private void showFileError(String path) {
+        String errorMessage = path + " not found.";
         JOptionPane.showMessageDialog(null,errorMessage,"File Error",JOptionPane.ERROR_MESSAGE);
+        System.exit(0);
     }
 }
